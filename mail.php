97cb415d723e76a82a5f76aa43d704bd8a53377a
Med El Anani <issimo1122@gmail.com>
2025-06-18 14:24:41 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -15,8 +15,8 @@ $mail = new PHPMailer();
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                                 //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'issimo181@gmail.com';                           //SMTP username
-    $mail->Password   = 'nqkt xysf nhov viuf';                        //SMTP password
+    $mail->Username   = 'YOUR_EMAIL';                           //SMTP username
+    $mail->Password   = 'SMTP_PASSWORD';                        //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
